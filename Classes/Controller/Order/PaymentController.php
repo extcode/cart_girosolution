8360a3bbabed40d677c105de24a3ac565153a6ce
Daniel Gohlke <daniel.gohlke@extco.de>
2024-12-13 16:06:18 +0100

    [TASK] Use GiroCheckout_SDK_Notify for response from payment gateway
    Relates: #14

--- Classes/Controller/Order/PaymentController.php
+++ Classes/Controller/Order/PaymentController.php
@@ -17,12 +17,15 @@ use Extcode\Cart\Domain\Repository\CartRepository;
 use Extcode\Cart\Domain\Repository\Order\ItemRepository as OrderItemRepository;
 use Extcode\Cart\Domain\Repository\Order\PaymentRepository;
 use Extcode\Cart\Service\SessionHandler;
+use Extcode\CartGirosolution\Configuration\CredentialLoaderRegistry;
 use Extcode\CartGirosolution\Event\Order\CancelEvent;
 use Extcode\CartGirosolution\Event\Order\FinishEvent;
+use girosolution\GiroCheckout_SDK\GiroCheckout_SDK_Notify;
+use InvalidArgumentException;
 use Psr\Http\Message\ResponseInterface;
-use TYPO3\CMS\Core\Messaging\AbstractMessage;
 use TYPO3\CMS\Core\Messaging\FlashMessage;
 use TYPO3\CMS\Core\Messaging\FlashMessageService;
+use TYPO3\CMS\Core\Type\ContextualFeedbackSeverity;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
@@ -37,16 +40,21 @@ class PaymentController extends ActionController
 
     protected array $pluginSettings;
 
+    private array $credentials = [];
+
     public function __construct(
         private readonly PersistenceManager $persistenceManager,
         private readonly SessionHandler $sessionHandler,
         private readonly CartRepository $cartRepository,
         private readonly OrderItemRepository $orderItemRepository,
         private readonly PaymentRepository $paymentRepository,
+        readonly CredentialLoaderRegistry $credentialLoaderRegistry,
     ) {}
 
     protected function initializeAction(): void
     {
+        $this->credentials = $this->credentialLoaderRegistry->getCredentials();
+
         $this->cartPluginSettings =
             $this->configurationManager->getConfiguration(
                 ConfigurationManagerInterface::CONFIGURATION_TYPE_FRAMEWORK,
@@ -64,14 +72,14 @@ class PaymentController extends ActionController
     {
         $this->cart = $this->loadCartByArgumentHash();
 
-        if (is_null($this->cart)) {
-            // todo: log and throw an exception
-        }
-
         $orderItem = $this->cart->getOrderItem();
 
-        if (GeneralUtility::_GET('gcResultPayment') === '4000') {
-            $dispatchEvent = $this->updatePaymentAndTransaction($orderItem, 'paid');
+        $clearingType = $this->getClearingType($orderItem);
+
+        $notify = $this->getParsedNotify($clearingType);
+
+        if ($notify->paymentSuccessful()) {
+            $dispatchEvent = $this->updatePaymentAndTransaction($notify, $orderItem, 'paid');
 
             if ($dispatchEvent) {
                 $finishEvent = new FinishEvent($this->cart->getCart(), $orderItem, $this->cartPluginSettings);
@@ -88,7 +96,7 @@ class PaymentController extends ActionController
             );
         }
 
-        $dispatchEvent = $this->updatePaymentAndTransaction($orderItem, 'canceled');
+        $dispatchEvent = $this->updatePaymentAndTransaction($notify, $orderItem, 'canceled');
 
         $this->restoreCartSession();
 
@@ -107,18 +115,18 @@ class PaymentController extends ActionController
         );
     }
 
-    public function notifyAction(): string
+    public function notifyAction(): void
     {
         $this->cart = $this->loadCartByArgumentHash();
 
-        if (is_null($this->cart)) {
-            return 'ERROR';
-        }
-
         $orderItem = $this->cart->getOrderItem();
 
-        if (GeneralUtility::_GET('gcResultPayment') === '4000') {
-            $dispatchEvent = $this->updatePaymentAndTransaction($orderItem, 'paid');
+        $clearingType = $this->getClearingType($orderItem);
+
+        $notify = $this->getParsedNotify($clearingType);
+
+        if ($notify->paymentSuccessful()) {
+            $dispatchEvent = $this->updatePaymentAndTransaction($notify, $orderItem, 'paid');
 
             if ($dispatchEvent) {
                 $orderItem = $this->cart->getOrderItem();
@@ -126,7 +134,7 @@ class PaymentController extends ActionController
                 $this->eventDispatcher->dispatch($finishEvent);
             }
         } else {
-            $dispatchEvent = $this->updatePaymentAndTransaction($orderItem, 'canceled');
+            $dispatchEvent = $this->updatePaymentAndTransaction($notify, $orderItem, 'canceled');
 
             if ($dispatchEvent) {
                 $orderItem = $this->cart->getOrderItem();
@@ -135,17 +143,17 @@ class PaymentController extends ActionController
             }
         }
 
-        return 'OK';
+        $notify->sendOkStatus();
     }
 
-    private function updatePaymentAndTransaction(Item $orderItem, string $status): bool
+    private function updatePaymentAndTransaction($notify, Item $orderItem, string $status): bool
     {
         $dispatchEvent = false;
 
         $payment = $orderItem->getPayment();
 
         foreach ($payment->getTransactions() as $transaction) {
-            if ($transaction->getTxnId() === GeneralUtility::_GET('gcReference')) {
+            if ($transaction->getTxnId() === ($notify->getResponseParam('gcReference'))) {
                 break;
             }
         }
@@ -153,8 +161,8 @@ class PaymentController extends ActionController
             throw new Exception('No transation found!', 1731616906);
         }
 
-        $transaction->setExternalStatusCode(GeneralUtility::_GET('gcResultPayment'));
-        $transaction->setNote(GeneralUtility::_GET('gcBackendTxId'));
+        $transaction->setExternalStatusCode($notify->getResponseParam('gcResultPayment'));
+        $transaction->setNote($notify->getResponseParam('gcBackendTxId'));
 
         if ($transaction->getStatus() !== $status) {
             $transaction->setStatus($status);
@@ -180,7 +188,7 @@ class PaymentController extends ActionController
                 'CartGirosolution'
             ),
             '',
-            AbstractMessage::ERROR,
+            ContextualFeedbackSeverity::ERROR,
             true
         );
 
@@ -189,7 +197,7 @@ class PaymentController extends ActionController
         $messageQueue->enqueue($flashMessage);
     }
 
-    private function loadCartByArgumentHash(): ?Cart
+    private function loadCartByArgumentHash(): Cart
     {
         if ($this->request->hasArgument('hash')) {
             $hash = $this->request->getArgument('hash');
@@ -200,20 +208,63 @@ class PaymentController extends ActionController
                 $this->cartRepository->setDefaultQuerySettings($querySettings);
 
                 $cart = $this->cartRepository->findOneBy(['sHash' => $hash]);
-                if ($cart instanceof Cart) {
-                    return $cart;
+                if (!$cart instanceof Cart) {
+                    throw new InvalidArgumentException('Invalid Hash!', 1734097682);
                 }
+
+                return $cart;
             }
         }
 
-        return null;
+        throw new InvalidArgumentException('No Hash!', 1734098478);
     }
 
-    protected function restoreCartSession(): void
+    private function restoreCartSession(): void
     {
         $cart = $this->cart->getCart();
         $cart->resetOrderNumber();
         $cart->resetInvoiceNumber();
         $this->sessionHandler->writeCart($this->cartPluginSettings['settings']['cart']['pid'], $cart);
+    }
+
+    private function getClearingType(?Item $orderItem): string
+    {
+        $payment = $orderItem->getPayment();
+        $provider = $payment->getProvider();
+        [$provider, $clearingType] = array_pad(explode('_', (string)$provider), 2, '');
+
+        return $clearingType;
+    }
+
+    private function getNotify(string $clearingType): GiroCheckout_SDK_Notify
+    {
+        return match ($clearingType) {
+            'CREDITCARD' => new GiroCheckout_SDK_Notify('creditCardTransaction'),
+            'GIROPAY' => new GiroCheckout_SDK_Notify('giropayTransaction'),
+            'PAYPAL' => new GiroCheckout_SDK_Notify('paypalTransaction'),
+            'PAYDIREKT' => new GiroCheckout_SDK_Notify('paydirektTransaction'),
+            default => throw new InvalidArgumentException('Invalid Clearint Type', 1734099627),
+        };
+    }
+
+    private function getPaymentConfiguration(string $clearingType): array
+    {
+        return match ($clearingType) {
+            'CREDITCARD' => $this->credentials['creditCard'],
+            'GIROPAY' => $this->credentials['giropay'],
+            'PAYPAL' => $this->credentials['paypal'],
+            'PAYDIREKT' => $this->credentials['paydirekt'],
+            default => throw new InvalidArgumentException('Invalid Clearint Type', 1734099627),
+        };
+    }
+
+    private function getParsedNotify(string $clearingType): GiroCheckout_SDK_Notify
+    {
+        $notify = $this->getNotify($clearingType);
+        $paymentConf = $this->getPaymentConfiguration($clearingType);
+        $notify->setSecret($paymentConf['password']);
+        $notify->parseNotification($_GET);
+
+        return $notify;
     }
 }
